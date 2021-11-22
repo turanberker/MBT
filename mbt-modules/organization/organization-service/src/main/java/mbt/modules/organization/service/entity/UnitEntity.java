fb96cb9f845226cfcd9c59c9d03f99cd7a091f2e
Berker Turan <turanberker@yahoo.com>
2021-11-22 20:48:07 +0300

    StatusEntityNamingBean

--- mbt-modules/organization/organization-service/src/main/java/mbt/modules/organization/service/entity/UnitEntity.java
+++ mbt-modules/organization/organization-service/src/main/java/mbt/modules/organization/service/entity/UnitEntity.java
@@ -2,6 +2,7 @@ package mbt.modules.organization.service.entity;
 
 
 import mbt.core.service.entity.BaseEntity;
+import mbt.core.service.entity.StatusEntity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
@@ -17,6 +18,10 @@ public class UnitEntity extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "UNIT_GROUP_ID", foreignKey = @ForeignKey(name = "FK_UNIT_GROUP0"))
     private UnitGrupEntity group;
+
+    @ManyToOne
+    @JoinColumn(name = "STATUS_ID", foreignKey = @ForeignKey(name = "FK_UNIT_STATUS0"))
+    private StatusEntity status;
 
     @ManyToOne
     @JoinColumn(name = "PARENT_UNIT_ID", foreignKey = @ForeignKey(name = "FK_UNIT_UNIT0"))
@@ -44,5 +49,13 @@ public class UnitEntity extends BaseEntity {
 
     public void setGroup(UnitGrupEntity group) {
         this.group = group;
+    }
+
+    public StatusEntity getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEntity status) {
+        this.status = status;
     }
 }
